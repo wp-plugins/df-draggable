c810be17b29c1d64f2b89ae12534e486aff022c2
domfallowsweb <domfallowsweb@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-12-21 10:18:26 +0000

    Adding first complete upload and tagging version 1.0
    git-svn-id: https://plugins.svn.wordpress.org/df-draggable/trunk@478435 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ admin.php
@@ -0,0 +1,269 @@
+<?php
+/**
+ * @package DF Draggable
+ * @version 1.0
+ */
+/*
+Plugin Name: DF Draggable 
+Version: 1.0
+Plugin URI: http://wordpress.org/extend/plugins/df-draggable/
+Description: DF Draggable is a plugin for Wordpress that enables you to make elements draggable utilising jQuery UI draggable
+Author: Dominic Fallows
+Author URI: http://www.dominicfallows.com/apps-plugins/df-draggable/
+License: GPLv2 or later
+Tags: jquery, jquery ui, drag, drop, draggable
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+define('DF_DRAGGABLE_PAGE_TITLE', 'DF Draggable Settings');
+define('DF_DRAGGABLE_MENU_TITLE', 'DF Draggable');
+
+function df_draggable_admin_init() {
+    wp_register_style('df-draggable-admin.css', DF_DRAGGABLE_PLUGIN_URL . 'df-draggable-admin.css');
+    wp_enqueue_style('df-draggable-admin.css');    
+}
+
+function df_draggable_admin_enqueue_scripts( $hook_suffix ) {
+    
+    $df_draggable_admin_pages = array(
+                                    'settings_page_df-draggable'                               
+                                );
+       
+    if(in_array($hook_suffix, $df_draggable_admin_pages)) {
+        wp_enqueue_style( 'farbtastic' );
+        wp_enqueue_script( 'farbtastic' );      
+    }
+}
+
+add_action('admin_init', 'df_draggable_admin_init');
+
+function admin_load_into_head() { ?>
+<script type="text/javascript">
+ 
+  jQuery(document).ready(function() {
+    jQuery('#ilctabscolorpicker').hide();
+    jQuery('#ilctabscolorpicker').farbtastic("#dragBGcolor");
+    jQuery("#dragBGcolor").click(function(){jQuery('#ilctabscolorpicker').slideToggle()});
+  });
+ 
+</script>
+<?php } 
+
+function df_draggable_menu() {
+    
+    $plugin_menu = add_submenu_page('options-general.php', DF_DRAGGABLE_PAGE_TITLE, DF_DRAGGABLE_MENU_TITLE, 'manage_options', 'df-draggable', 'df_draggable_settings_page');
+    
+    add_action( 'admin_head-'. $plugin_menu, 'admin_load_into_head' );
+    
+    add_action( 'admin_enqueue_scripts', 'df_draggable_admin_enqueue_scripts' ); //call the admin javascript files
+    
+}
+
+add_action( 'admin_menu', 'df_draggable_menu' );
+
+
+function df_draggable_createform($form_id, $action, $button_text = "Submit", $errors = array(), $makeDraggableClassSetting = null, $containment = "window", $dragBGcolor = "#efefef", $snap = true) {
+  
+    if (!empty($errors)) {    
+        echo '<div id="df-admin-errors" class="df-admin-errors"><ul>';
+        foreach ($errors as $error) {
+            echo "<li>" . $error . "</li>";
+        }
+        echo '</ul></div>';
+    }
+    
+    
+    echo '<form id="'.$form_id.'" method="post" action="'.$action.'">';
+      
+    echo '<table cellspacing="0" cellpadding="0" class="df-admin-update-table">';
+    
+    echo '<tr>
+            <td class="df-admin-update-table-th">Class of element to make draggable</td>
+            <td><input id="makeDraggableClass" name="makeDraggableClass" type="text" maxlength="255" value="'. stripslashes($makeDraggableClassSetting) .'" /><br />
+                <small>If you want to disable the draggable content, please enter \'none\' (without quotation marks)</small></td>
+        </tr>';  
+    
+    
+    echo '<tr>
+            <td class="df-admin-update-table-th">Where are the elements allowed to be dragged (contained)?</td>
+            <td>
+            <select id="containment" name="containment">
+                <option id="parent" value="parent" ';
+                if ($containment == "parent") { echo "selected='selected'"; }
+                echo '>Parent</option>
+                    
+                <option id="document" value="document" ';
+                if ($containment == "document") { echo "selected='selected'"; }
+                echo '>Document</option>
+                    
+                <option id="window" value="window" ';
+                if ($containment == "window") { echo "selected='selected'"; }
+                echo '>Window</option>
+            </select>
+            <p><small>
+            <strong>Parent: </strong> - Constrains the draggable element to its parent element<br />
+            <strong>Document: </strong> - Constrains the draggable element to the HTML page height/width<br />
+            <strong>Window: </strong> - Constrains the draggable element to the viewable browser window
+            </small></p>          
+            </td>
+        </tr>';  
+
+    echo '<tr>
+            <td class="df-admin-update-table-th">Background color of the draggable element, whilst being dragged</td>
+            <td><input id="dragBGcolor" name="dragBGcolor" type="text" maxlength="255" value="'. stripslashes($dragBGcolor) .'" />
+                <div id="ilctabscolorpicker"></div>
+            </td>
+            </tr>';  
+    
+    echo '<tr>
+            <td class="df-admin-update-table-th">Do you want the draggable elements to snap (align) with other dragged elements?</td>
+            <td>
+            <select id="snap" name="snap">
+                <option id="true" value="true" ';
+                if ($snap == "true") { echo "selected='selected'"; }
+                echo '>True</option>
+                    
+                <option id="false" value="false" ';
+                if ($snap == "false") { echo "selected='selected'"; }
+                echo '>False</option>
+               
+            </select>   
+            <p><small>Elements will snap together when close to each other</small></p>
+            </td>
+        </tr>'; 
+                
+    
+    echo '<tr>
+            <td class="df-admin-update-table-th"></td>
+            <td><input id="submit" name="submit" type="submit" value="'.$button_text.'" /></td>
+        </tr>';
+    
+    echo '</table>';
+    
+    echo '</form>';
+            
+}
+function df_draggable_settings_page() { ?>
+
+<div id="df_draggable_admin">
+    
+    <h2><?php echo DF_DRAGGABLE_PAGE_TITLE; ?></h2>
+    
+    <p>This plugin will make any element draggable: Images <code>img</code>, 
+        Divs <code>div</code>, Tables <code>table</code>, 
+        Headings <code>h1, h2, h3, h4 ...</code>, Paragraphs <code>p</code>, 
+        Lists <code>ul, ol</code>, Links <code>a</code>, etc...</p>
+
+    <p>Use the form below to update the settings for draggable elements</p>
+
+    <?php 
+        if (isset($_POST['submit'])) { 
+
+            $errors = array();
+
+            if (empty($_POST['makeDraggableClass'])) {
+                $errors[] = "You must enter a class to make draggable, default is 'post'";
+            }  
+
+            if (count($errors) != 0) {
+
+                df_draggable_createform("df-admin-update", 
+                                        currentURL(), 
+                                        "Save Settings",
+                                        $errors,
+                                        $_POST['makeDraggableClass'],
+                                        $_POST['containment'],
+                                        $_POST['dragBGcolor'],
+                                        $_POST['snap']
+                                        );
+
+            } else {
+                
+                $df_draggable_options = array();
+                $df_draggable_options['makeDraggableClass']     = $_POST['makeDraggableClass'];
+                $df_draggable_options['containment']            = $_POST['containment'];
+                $df_draggable_options['dragBGcolor']            = $_POST['dragBGcolor'];
+                $df_draggable_options['snap']                   = $_POST['snap'];
+                
+                /* UPDATE SETTINGS */
+                
+                try {
+                    update_option('df-draggable-options', serialize($df_draggable_options));
+                    echo "<p class='df-admin-success'>The settings have been updated succesffuly.</p>";
+                    df_draggable_createform("df-admin-update", 
+                                        currentURL(), 
+                                        "Save Settings",
+                                        $errors,
+                                        $_POST['makeDraggableClass'],
+                                        $_POST['containment'],
+                                        $_POST['dragBGcolor'],
+                                        $_POST['snap']
+                                        );
+                    
+                } catch (Exception $e) {
+                    
+                    
+                    echo "<p class='df-admin-errors'>There was an error updating the settings. {$e->getMessage()}</p>";
+                    df_draggable_createform("df-admin-update", 
+                                        currentURL(), 
+                                        "Save Settings",
+                                        $errors,
+                                        $_POST['makeDraggableClass'],
+                                        $_POST['containment'],
+                                        $_POST['dragBGcolor'],
+                                        $_POST['snap']
+                                        );
+                    
+                } 
+
+
+
+
+            }
+
+
+
+        } else {
+            
+            $df_draggable_options = unserialize(get_option('df-draggable-options'));
+            
+            if(!empty($df_draggable_options)) {
+                $makeDraggableClass = $df_draggable_options['makeDraggableClass'];
+                $containment        = $df_draggable_options['containment']; 
+                $dragBGcolor        = $df_draggable_options['dragBGcolor'];
+                $snap               = $df_draggable_options['snap'];
+            } else {
+                $makeDraggableClass = "none";
+                $containment        = null;
+                $dragBGcolor        = null;
+                $snap               = null;
+            }
+            
+            df_draggable_createform("df-admin-update", 
+                                        currentURL(), 
+                                        "Save Settings",
+                                        null,
+                                        $makeDraggableClass,
+                                        $containment,
+                                        $dragBGcolor,
+                                        $snap
+                                    );
+        }     
+        ?>
+
+    </div>
+
+<?php } //end df_draggable_settings_page() ?>
---
+++ df-draggable.php
@@ -0,0 +1,159 @@
+<?php
+/**
+ * @package DF Draggable
+ * @version 1.0
+ */
+/*
+Plugin Name: DF Draggable 
+Version: 1.0
+Plugin URI: http://wordpress.org/extend/plugins/df-draggable/
+Description: DF Draggable is a plugin for Wordpress that enables you to make elements draggable utilising jQuery UI draggable
+Author: Dominic Fallows
+Author URI: http://www.dominicfallows.com/apps-plugins/df-draggable/
+License: GPLv2 or later
+Tags: jquery, jquery ui, drag, drop, draggable
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+
+function define_globals() {
+    global $wpdb;
+    define('DF_DRAGGABLE_VERSION', '1.0');  
+    define('DF_DRAGGABLE_PLUGIN_URL', plugin_dir_url( __FILE__ ));
+}
+
+function init_activation() {
+    
+    //see if options already exist
+    $df_draggable_options = unserialize(get_option('df-draggable-options'));
+    
+    
+    if (!is_array($df_draggable_options)) {
+        $df_draggable_options = array();
+        $df_draggable_options['makeDraggableClass']     = "post";
+        $df_draggable_options['containment']            = "window";
+        $df_draggable_options['dragBGcolor']            = "#efefef";
+        $df_draggable_options['snap']                   = true;
+        //save the init options
+        update_option('df-draggable-options', serialize($df_draggable_options));
+    }
+    
+
+    //To call the option:
+    //get_option('my-plugin-options') == "" ? "" : $new = unserialize(get_option('my-plugin-options')); 
+
+}
+
+define_globals();
+require_once("includes/functions.php");
+register_activation_hook(__FILE__, 'init_activation');
+
+// direct file request handling
+if (!function_exists('add_action')) { echo "You shouldn't be here!"; exit; }
+
+//admin handling
+if (is_admin()) { require_once dirname( __FILE__ ) . '/admin.php'; }
+
+
+//init
+function df_draggable_init() {
+ 
+	wp_register_style('df-draggable-view.css', DF_DRAGGABLE_PLUGIN_URL . 'df-draggable-view.css');
+	wp_enqueue_style('df-draggable-view.css');
+        
+        wp_enqueue_script("jquery");
+        //wp_enqueue_script("df-draggable-view", DF_DRAGGABLE_PLUGIN_URL .'includes/df-draggable-view.js', array("jquery"), "1.7.1",1);	
+        
+        //wp_register_script('df-draggable-view', DF_DRAGGABLE_PLUGIN_URL .'includes/df-draggable-view.js');
+        //wp_enqueue_script('df-draggable-view');
+        
+}
+
+add_action('init', 'df_draggable_init');
+
+//view handling
+function load_into_head() {
+    
+    $df_draggable_options = unserialize(get_option('df-draggable-options'));
+    
+    if(!empty($df_draggable_options)) {
+        $makeDraggableClass     = $df_draggable_options['makeDraggableClass'];
+        $containment            = $df_draggable_options['containment']; 
+        $dragBGcolor            = $df_draggable_options['dragBGcolor'];
+        $snap                   = $df_draggable_options['snap'];
+    } else {
+        $makeDraggableClass = null;
+    }
+?>
+    
+    <script type="text/javascript">
+    
+    if (typeof jQuery == 'undefined') {
+        var head= document.getElementsByTagName('head')[0];
+        var script= document.createElement('script');
+        script.type= 'text/javascript';
+        script.src= 'http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js';
+        head.appendChild(script);
+    }
+    </script>
+    <script type="text/javascript">if (typeof $ != 'undefined') { $.noConflict(); }</script>
+    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.9/jquery-ui.min.js"></script>
+    <?php
+    if($makeDraggableClass !=null && $makeDraggableClass !="none") { ?>
+    <script type="text/javascript">
+        
+        jQuery(document).ready(function() {
+            
+            jQuery('.<?php echo $makeDraggableClass; ?>').draggable( {
+                containment: '<?php echo $containment; ?>',
+                cursor: 'move',
+                snap: <?php echo $snap; ?>
+            });
+            
+            jQuery('.<?php echo $makeDraggableClass; ?>').hover(function() {
+                jQuery(this).css('cursor','move');
+                }, function() {
+                    jQuery(this).css('cursor','auto');
+            });
+
+            
+        });
+    </script>
+    <style type="text/css">
+        .ui-draggable-dragging { background: <?php echo $dragBGcolor; ?>; }
+    </style>
+    <?php } ?>
+<?php
+      
+} 
+add_action( 'wp_head', 'load_into_head' );
+
+
+function df_draggable_view($content = '') {
+              
+    $df_draggable_html = "";
+    
+    $new_content = str_replace("[DF-DRAGGABLE]", $df_draggable_html, $content);
+    
+    return $new_content;
+    
+}
+
+add_filter('the_content', 'df_draggable_view');
+
+ 
+
+?>
